469fa44a9ba93277f734dfb2257ef5847a13bc48
Michael Wood <mwood@data-crush.com>
2017-10-06 07:00:04 -0400

    Added second example

--- src/App.js
+++ src/App.js
@@ -3,27 +3,77 @@ import React, { Component } from 'react'
 import ReactChartjs from './components/ReactChart.js'
 
 class App extends Component {
-  dataset (labels, min = 0, max = 100) {
+  dataset (n, min = 0, max = 100) {
     const data = []
 
-    for (let i = 0; i < labels.length; ++i) {
+    for (let i = 0; i < n; ++i) {
       data.push(Math.floor(Math.random() * 50))
     }
     return data
   }
   render () {
     const labels = ['Red', 'Pink', 'Purple', 'Deep Purple', 'Indigo', 'Blue', 'Light Blue']
-    const datasets = [{
+    const barDatasets = [{
       label: 'Material Colors',
-      data: this.dataset(labels)
+      data: this.dataset(labels.length)
     }]
+
+    const lineDatasets = [{
+      label: 'My First dataset',
+      borderColor: 'blue',
+      data: this.dataset(7),
+      fill: false
+    },
+    {
+      label: 'My Second dataset',
+      fill: false,
+      borderColor: 'red',
+      data: this.dataset(7)
+    }]
+
+    const lineOptions = {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart'
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: false
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true
+      },
+      scales: {
+        xAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Month'
+          }
+        }],
+        yAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Value'
+          }
+        }]
+      }
+    }
     return (
       <div className='App'>
         <ReactChartjs
           type='bar'
           labels={labels}
-          datasets={datasets}
-          options={{title: { display: true, text: 'Chart.js Line Chart' }}} />
+          datasets={barDatasets}
+          options={{title: { display: true, text: 'Chart.js Bar Chart' }}} />
+        <ReactChartjs
+          type='line'
+          labels={['January', 'February', 'March', 'April', 'May', 'June', 'July']}
+          datasets={lineDatasets}
+          options={lineOptions} />
       </div>
     )
   }
